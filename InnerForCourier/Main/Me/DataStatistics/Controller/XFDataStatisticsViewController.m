783dc2a984db34d11ac66af401ada709ed3657b2
Damon <liao0322@hotmail.com>
2017-03-21 19:55:07 +0800

    重构代码

--- InnerForCourier/Main/Me/DataStatistics/Controller/XFDataStatisticsViewController.m
+++ InnerForCourier/Main/Me/DataStatistics/Controller/XFDataStatisticsViewController.m
@@ -11,7 +11,7 @@
 
 #import "XFDataStatisticsRequest.h"
 #import "XFOrderChart.h"
-#import "DateValueFormatter.h"
+
 #import "InnerForCourier-Bridging-Header.h"
 #import "DayAxisValueFormatter.h"
 #import "NSDate+Extension.h"
@@ -115,77 +115,7 @@ static NSString * const CVCellID = @"CVCellID";
     [self.collectionView registerNib:[UINib nibWithNibName:NSStringFromClass([XFDataStatisticsCVCell class]) bundle:nil] forCellWithReuseIdentifier:CVCellID];
 }
 
-- (void)tempChart2 {
-    _chartView.delegate = self;
-    
-    _chartView.chartDescription.enabled = NO;
-    
-    _chartView.dragEnabled = YES;
-    [_chartView setScaleEnabled:YES];
-    _chartView.pinchZoomEnabled = NO;
-    _chartView.drawGridBackgroundEnabled = NO;
-    _chartView.highlightPerDragEnabled = YES;
-    
-    _chartView.backgroundColor = UIColor.whiteColor;
-    
-    _chartView.legend.enabled = NO;
-    
-    
-    ChartXAxis *xAxis = _chartView.xAxis;
-    xAxis.labelPosition = XAxisLabelPositionTopInside;
-    xAxis.labelFont = [UIFont fontWithName:@"HelveticaNeue-Light" size:10.f];
-    xAxis.labelTextColor = [UIColor colorWithRed:255/255.0 green:192/255.0 blue:56/255.0 alpha:1.0];
-    xAxis.drawAxisLineEnabled = NO;
-    xAxis.drawGridLinesEnabled = YES;
-    xAxis.centerAxisLabelsEnabled = YES;
-    xAxis.granularity = 3600.0;
-    xAxis.valueFormatter = [[DateValueFormatter alloc] init];
-    
-    
-    ChartYAxis *leftAxis = _chartView.leftAxis;
-    leftAxis.labelPosition = YAxisLabelPositionInsideChart;
-    leftAxis.labelFont = [UIFont fontWithName:@"HelveticaNeue-Light" size:12.f];
-    leftAxis.labelTextColor = [UIColor colorWithRed:51/255.0 green:181/255.0 blue:229/255.0 alpha:1.0];
-    leftAxis.drawGridLinesEnabled = YES;
-    leftAxis.granularityEnabled = YES;
-    leftAxis.axisMinimum = 0.0;
-    leftAxis.axisMaximum = 170.0;
-    leftAxis.yOffset = -9.0;
-    leftAxis.labelTextColor = [UIColor colorWithRed:255/255.0 green:192/255.0 blue:56/255.0 alpha:1.0];
-    
-    _chartView.rightAxis.enabled = NO;
-    
-    _chartView.legend.form = ChartLegendFormLine;
-    
-    [self setDataCount:30 range:100];
-}
 
-- (void)tempChart {
-    _chartView.delegate = self;
-    
-    _chartView.chartDescription.enabled = NO;
-    
-    _chartView.dragEnabled = YES;
-    [_chartView setScaleEnabled:YES];
-    _chartView.pinchZoomEnabled = YES;
-    _chartView.drawGridBackgroundEnabled = NO;
-    
-    _chartView.xAxis.gridLineDashLengths = @[@10.0, @10.0];
-    _chartView.xAxis.gridLineDashPhase = 0.f;
-    
-    ChartYAxis *leftAxis = _chartView.leftAxis;
-    [leftAxis removeAllLimitLines];
-    leftAxis.gridLineDashLengths = @[@5.f, @5.f];
-    leftAxis.drawZeroLineEnabled = NO;
-    leftAxis.drawLimitLinesBehindDataEnabled = YES;
-    
-    
-    _chartView.rightAxis.enabled = NO;
-    _chartView.legend.enabled = NO;
-    
-    [self setDataCount:30 range:100];
-    [_chartView animateWithXAxisDuration:2.5];
-}
 
 #pragma mark - Custom
 
@@ -235,60 +165,6 @@ static NSString * const CVCellID = @"CVCellID";
     }
     
 }
-
-
-
-
-- (void)setDataCount:(int)count range:(double)range
-{
-    NSTimeInterval now = [[NSDate date] timeIntervalSince1970];
-    NSTimeInterval hourSeconds = 3600.0;
-    
-    NSMutableArray *values = [[NSMutableArray alloc] init];
-    
-    NSTimeInterval from = now - (count / 2.0) * hourSeconds;
-    NSTimeInterval to = now + (count / 2.0) * hourSeconds;
-    
-    for (NSTimeInterval x = from; x < to; x += hourSeconds)
-    {
-        double y = arc4random_uniform(range) + 50;
-        [values addObject:[[ChartDataEntry alloc] initWithX:x y:y]];
-    }
-    
-    LineChartDataSet *set1 = nil;
-    if (_chartView.data.dataSetCount > 0)
-    {
-        set1 = (LineChartDataSet *)_chartView.data.dataSets[0];
-        set1.values = values;
-        [_chartView.data notifyDataChanged];
-        [_chartView notifyDataSetChanged];
-    }
-    else
-    {
-        set1 = [[LineChartDataSet alloc] initWithValues:values label:@"DataSet 1"];
-        set1.axisDependency = AxisDependencyLeft;
-        set1.valueTextColor = [UIColor colorWithRed:51/255.0 green:181/255.0 blue:229/255.0 alpha:1.0];
-        set1.lineWidth = 1.5;
-        set1.drawCirclesEnabled = NO;
-        set1.drawValuesEnabled = NO;
-        set1.fillAlpha = 0.26;
-        set1.fillColor = [UIColor colorWithRed:51/255.0 green:181/255.0 blue:229/255.0 alpha:1.0];
-        set1.highlightColor = [UIColor colorWithRed:224/255.0 green:117/255.0 blue:117/255.0 alpha:1.0];
-        set1.drawCircleHoleEnabled = NO;
-        
-        NSMutableArray *dataSets = [[NSMutableArray alloc] init];
-        [dataSets addObject:set1];
-        
-        LineChartData *data = [[LineChartData alloc] initWithDataSets:dataSets];
-        [data setValueTextColor:UIColor.whiteColor];
-        [data setValueFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:9.0]];
-        
-        _chartView.data = data;
-    }
-}
-
-
-
 
 
 
